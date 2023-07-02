fe709ded8fc60e5deca16b4c6f7c0c368c96796f
Aminabibi12 <bibiamina453@gmail.com>
2023-07-02 23:41:46 +0200

    second commit

--- file1.js
+++ file1.js
@@ -1,7 +1,29 @@
-async function fetchData() {
+async function fetchData1() {
+    let response = await fetch('https://dog.ceo/api/breeds/image/random');
+    let data = await response.json();
+    console.log(data);
+    document.getElementById('image').innerHTML = `<img src = "${data.message}">`;
+}
+
+fetchData1()
+
+
+async function fetchData2() {
     let response = await fetch('https://catfact.ninja/fact');
     let data = await response.json();
     console.log(data);
     document.getElementById('fact').innerHTML = data.fact;
 }
-fetchData()
+
+fetchData2()
+
+async function fetchData3() {
+    let response = await fetch('http://universities.hipolabs.com/search?country=United+States');
+    let data = await response.json();
+    console.log(data);
+
+    let names = data.map(university => university.name);
+    document.getElementById('list').innerHTML = names.join(',' );
+}
+
+fetchData3()
